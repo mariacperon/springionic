f6d500a56ce32d9b3dbd60f50f3635a19345afbc
mariacperon <mariaclaraperon@gmail.com>
2022-01-06 16:31:17 -0300

    creating repository

---
+++ src/main/java/com/cursosp/projetosp/ProjetospApplication.java
@@ -0,0 +1,13 @@
+package com.cursosp.projetosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetospApplication.class, args);
+	}
+
+}
