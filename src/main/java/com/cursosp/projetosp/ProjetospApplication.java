b8007ad05dd68948e3a84767cdee3224132fe8d9
mariacperon <mariaclaraperon@gmail.com>
2022-01-10 15:46:27 -0300

    instanciando categorias

--- src/main/java/com/cursosp/projetosp/ProjetospApplication.java
+++ src/main/java/com/cursosp/projetosp/ProjetospApplication.java
@@ -1,13 +1,30 @@
 package com.cursosp.projetosp;
 
+import com.cursosp.projetosp.domain.Categoria;
+import com.cursosp.projetosp.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ProjetospApplication {
+public class ProjetospApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetospApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
